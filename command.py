f57c893433e6bd82d04eae571d6c4ee470c2c867
Danila <dan_crysis@mail.ru>
2022-10-05 21:54:05 +0300

    Dz_8

---
+++ command.py
@@ -0,0 +1,57 @@
+
+from ui import hello_user
+from studens import students_list
+from stunent_class import class_list
+from random import  randint
+def search():
+    while True:
+        hello_user()
+        n = int(input())
+        if n ==1:
+            with open('students.csv', 'r', encoding = 'utf_8') as f:
+                    for line in f.readlines():
+                        print(line)
+            input('\nНажмите enter')
+
+        elif n == 2:
+            with open('class.csv', 'r', encoding = 'utf_8') as f:
+                    for line in f.readlines():
+                        print(line)
+            input('\nНажмите enter')
+
+        elif n == 3:
+            answer = input('Введите фамилию ученика: ').title()
+            with open('students.csv', 'r', encoding='utf_8') as f:
+                for line in f.readlines():
+                    if answer in line:
+                        a = line.split()
+                        print(a[3])
+
+        
+        elif n == 4:
+            f = open('students.csv','a+', encoding = 'utf_8')
+            new_contact = input('Введите данные(ФИ, год рождения, успеваемость, ID) ').split(' ')
+            f.write('\n'+'\t'.join(new_contact))
+            f.close()
+            # students_list['ID'] = new_contact[0]
+            input('\nНажмите enter')
+
+        elif n == 5:
+            answer = input("Введите Фамилию: ")
+            with open('class.csv', 'r', encoding='utf_8') as f:
+                a = len(f.readlines())
+
+            f = open('students.csv', 'r', encoding='utf_8')
+            for line in f.readlines():
+                if answer in line:
+                    with open('class.csv', 'r', encoding='utf_8') as f:
+                        print(f.readlines()[randint(1,a-1)])
+
+
+
+
+
+        elif n == 6:
+            print('До встречи')
+            exit()
+
